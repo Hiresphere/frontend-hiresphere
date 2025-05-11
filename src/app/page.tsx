c2d270ea10d9046d0107c3368c669b8b0ba9b436
Anshumohan Acharya <anshumohanacharya19@gmail.com>
2025-05-12 00:40:23 +0530

    [SETUP]: add auth proxy and update query in page component
    Add a new proxy rule in next.config.ts to route `/auth/:path*` requests to NGINX for auth-service.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@
 
 import { gql, useQuery } from '@apollo/client';
 
-const HELLO_QUERY = gql`
+const AUTH_QUERY = gql`
   query {
-    hello
+    authPing
   }
 `;
 
 export default function TestPage() {
-    const { data, loading, error } = useQuery(HELLO_QUERY);
+    const { data, loading, error } = useQuery(AUTH_QUERY);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
 
-    return <h1>{data.hello}</h1>;
+    return <h1>{data.authPing}</h1>;
 }
